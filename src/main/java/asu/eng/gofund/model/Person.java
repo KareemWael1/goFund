08de0e618f088079ea707df41852b833ef96162d
Zaituny <mohamedinv.580@gmail.com>
2024-11-02 11:13:07 +0200

    added setters and getters for id attribute

--- src/main/java/asu/eng/gofund/model/Person.java
+++ src/main/java/asu/eng/gofund/model/Person.java
@@ -7,4 +7,12 @@ import jakarta.persistence.Id;
 public class Person {
     @Id
     private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
