a8352d5e5b4166b99d996f22679f136468723900
KareemWael1 <kareem25102002@gmail.com>
2025-01-14 19:26:14 +0200

    Email Service facade

--- src/main/java/asu/eng/gofund/controller/EmailNotification.java
+++ src/main/java/asu/eng/gofund/controller/EmailNotification.java
@@ -1,7 +1,18 @@
 package asu.eng.gofund.controller;
 
+import asu.eng.gofund.services.EmailFacade;
+
+import javax.mail.MessagingException;
+
 public class EmailNotification {
     public static void sendEmail(String to, String subject, String body) {
+        try {
+            EmailFacade.sendEmail(to, subject, body);
+        }
+        catch (MessagingException e) {
+            System.out.println("Failed to send email to " + to + " with subject: " + subject + " and body: " + body + " due to " + e.getMessage());
+            e.printStackTrace();
+        }
         System.out.println("Email sent to " + to + " with subject: " + subject + " and body: " + body);
     }
 }
