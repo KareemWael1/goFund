ec6fb1f7472d878832e58d54467010afeb8785cd
Mustafa U. Abdelrahman <mustafausama@outlook.com>
2025-01-17 01:13:44 +0200

    Fix donation integration

--- src/main/java/asu/eng/gofund/State/DonationClosed.java
+++ src/main/java/asu/eng/gofund/State/DonationClosed.java
@@ -7,10 +7,10 @@ import asu.eng.gofund.model.Donation;
 public class DonationClosed implements IDonationState{
     @Override
     public void NextState(Donation donation) {
-        EmailNotificationService emailNotificationService = new EmailNotificationService();
-        emailNotificationService.sendNotification(userRepo.findById(campaign.getStarterId()).get().getEmail(),"Donation Successful", campaign.getName(), amount);
-        SMSNotificationService smsNotification = new SMSNotificationService();
-        smsNotification.sendNotification(userRepo.findById(campaign.getStarterId()).get().getPhoneNumber(), "Donation Successful", campaign.getName(), amount);
+//        EmailNotificationService emailNotificationService = new EmailNotificationService();
+//        emailNotificationService.sendNotification(userRepo.findById(campaign.getStarterId()).get().getEmail(),"Donation Successful", campaign.getName(), amount);
+//        SMSNotificationService smsNotification = new SMSNotificationService();
+//        smsNotification.sendNotification(userRepo.findById(campaign.getStarterId()).get().getPhoneNumber(), "Donation Successful", campaign.getName(), amount);
     }
 
     @Override
