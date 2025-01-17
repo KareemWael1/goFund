b4319ba1753fd6fdffd9cc9d4e251abd4b74bf11
Ahmed Tarek <7madak10@gmail.com>
2025-01-17 21:51:39 +0200

    Fixed a typo in getPaymentStrategy method

--- src/main/java/asu/eng/gofund/model/Donation.java
+++ src/main/java/asu/eng/gofund/model/Donation.java
@@ -118,7 +118,7 @@ public abstract class Donation {
         isRefunded = refunded;
     }
 
-    public String getpaymentStrategy(){
+    public String getPaymentStrategy(){
         return paymentStrategy;
     }
 
