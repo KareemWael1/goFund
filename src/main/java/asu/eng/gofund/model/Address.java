01f959e7226a7427f350642545e8a3425fda905d
Ahmed Tarek <7madak10@gmail.com>
2025-01-17 22:53:25 +0200

    delete setters getters

--- src/main/java/asu/eng/gofund/model/Address.java
+++ src/main/java/asu/eng/gofund/model/Address.java
@@ -17,30 +17,6 @@ public class Address {
     public Address() {
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Long getParentId() {
-        return parentId;
-    }
-
-    public void setParentId(Long parentId) {
-        this.parentId = parentId;
-    }
-
     public Address(Long id, String name, Long parentId) {
         this.id = id;
         this.name = name;
