cba7789e378d15c7eaa422fa8e654d7bb39710dd
KareemWael1 <kareem25102002@gmail.com>
2025-01-17 22:06:00 +0200

    Fawry Payment Support

--- src/main/java/asu/eng/gofund/controller/Payment/FawryPayment.java
+++ src/main/java/asu/eng/gofund/controller/Payment/FawryPayment.java
@@ -3,16 +3,10 @@ package asu.eng.gofund.controller.Payment;
 import java.util.Map;
 
 public class FawryPayment implements IPaymentStrategy{
-    private String fawryCode;
-    private String fawryAccount;
-    private String fawryPassword;
 
     @Override
     public boolean executePayment(Map<String, String> paymentInfo, double amount) {
-        fawryCode = paymentInfo.get("fawryCode");
-        fawryAccount = paymentInfo.get("fawryAccount");
-        fawryPassword = paymentInfo.get("fawryPassword");
-
-        return true;
+        IPaymentAdapter paymentAdapter = new FawryPaymentAdapter();
+        return paymentAdapter.executePayment(paymentInfo, amount);
     }
 }
---
+++ src/main/java/asu/eng/gofund/controller/Payment/FawryPaymentAdapter.java
@@ -0,0 +1,70 @@
+package asu.eng.gofund.controller.Payment;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import com.google.gson.Gson;
+
+public class FawryPaymentAdapter implements IPaymentAdapter {
+    private static final String apiUrl = "https://atfawry.fawrystaging.com/ECommerceWeb/Fawry/payments/charge";
+
+    @Override
+    public boolean executePayment(Map<String, String> paymentInfo, double amount) {
+        try {
+            Map<String, String> params = new HashMap<>();
+            params.put("fawryCode", paymentInfo.get("fawryCode"));
+            params.put("fawryAccount", paymentInfo.get("fawryAccount"));
+            params.put("fawryPassword", paymentInfo.get("fawryPassword"));
+            params.put("amount", String.valueOf(amount));
+            params.put("currency", paymentInfo.getOrDefault("currency", "EGP"));
+            params.put("description", "Fawry Payment");
+
+            Gson gson = new Gson();
+            String jsonInputString = gson.toJson(params);
+            String response = sendPostRequest(apiUrl, jsonInputString);
+
+            if (response.contains("\"status\":\"success\"")) {
+                return true;
+            } else {
+                System.out.println("Payment failed with response: " + response);
+                return false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private String sendPostRequest(String urlString, String jsonInputString) throws Exception {
+        HttpURLConnection connection = getHttpURLConnection(urlString, jsonInputString);
+
+        try (InputStreamReader reader = new InputStreamReader(connection.getInputStream(), "utf-8")) {
+            char[] buffer = new char[1024];
+            StringBuilder response = new StringBuilder();
+            int bytesRead;
+            while ((bytesRead = reader.read(buffer)) != -1) {
+                response.append(buffer, 0, bytesRead);
+            }
+            return response.toString();
+        }
+    }
+
+    private static HttpURLConnection getHttpURLConnection(String urlString, String jsonInputString) throws IOException {
+        URL url = new URL(urlString);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setDoOutput(true);
+
+        try (OutputStream os = connection.getOutputStream()) {
+            byte[] input = jsonInputString.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+        return connection;
+    }
+}
